1b106615a0a1273d35ff2da6ba28b81969e39d78
Oscar <odmarulandab@unal.edu.co>
2025-02-17 16:44:38 -0500

    sumar dos numeros terminado

--- app/src/main/java/com/example/addnumbers/MainActivity.kt
+++ app/src/main/java/com/example/addnumbers/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.addnumbers
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var numero1 = findViewById<EditText>(R.id.txtNumber1)
+        var numero2 = findViewById<EditText>(R.id.txtNumber2)
+        var boton1 = findViewById<Button>(R.id.sumarBoton)
+        var resultado = findViewById<TextView>(R.id.resultado)
+
+        boton1.setOnClickListener{
+
+            resultado.text = (numero1.text.toString().toInt()+numero2.text.toString().toInt()).toString()
         }
     }
 }
